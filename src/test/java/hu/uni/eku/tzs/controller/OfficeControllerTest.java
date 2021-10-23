069bf9a43a1630e6ac8bd278e9804946009d2f53
mviktor02 <mviktor2210@gmail.com>
2021-10-23 17:31:05 +0200

    fix(#10): add GIVEN/WHEN/THEN comments to tests

--- src/test/java/hu/uni/eku/tzs/controller/OfficeControllerTest.java
+++ src/test/java/hu/uni/eku/tzs/controller/OfficeControllerTest.java
@@ -35,71 +35,77 @@ class OfficeControllerTest {
 
     @Test
     void readAllSanFrancisco() {
+        // given
         when(officeManager.readAll()).thenReturn(List.of(TestDataProvider.getSanFranciscoOfficeModel()));
         when(officeMapper.office2officeDto(any())).thenReturn(TestDataProvider.getSanFranciscoOfficeDto());
-
+        // when
         Collection<OfficeDto> expected = List.of(TestDataProvider.getSanFranciscoOfficeDto());
 
         Collection<OfficeDto> actual = controller.readAllOffices();
-
+        // then
         assertThat(actual).usingRecursiveComparison().isEqualTo(expected);
     }
 
     @Test
     void createOfficeSanFrancisco() throws OfficeAlreadyExistsException {
+        // given
         OfficeDto sanFranciscoDto = TestDataProvider.getSanFranciscoOfficeDto();
         Office sanFrancisco = TestDataProvider.getSanFranciscoOfficeModel();
         when(officeMapper.officeDto2Office(sanFranciscoDto)).thenReturn(sanFrancisco);
         when(officeManager.record(sanFrancisco)).thenReturn(sanFrancisco);
         when(officeMapper.office2officeDto(sanFrancisco)).thenReturn(sanFranciscoDto);
-
+        // when
         OfficeDto actual = controller.create(sanFranciscoDto);
-
+        // then
         assertThat(actual).usingRecursiveComparison()
                 .isEqualTo(sanFranciscoDto);
     }
 
     @Test
     void createOfficeThrowsAlreadyExistsException() throws OfficeAlreadyExistsException {
+        // given
         OfficeDto sanFranciscoDto = TestDataProvider.getSanFranciscoOfficeDto();
         Office sanFrancisco = TestDataProvider.getSanFranciscoOfficeModel();
         when(officeMapper.officeDto2Office(sanFranciscoDto)).thenReturn(sanFrancisco);
         when(officeManager.record(sanFrancisco)).thenThrow(new OfficeAlreadyExistsException());
-
+        // when
         assertThatThrownBy(() -> controller.create(sanFranciscoDto))
                 .isInstanceOf(ResponseStatusException.class);
     }
 
     @Test
     void updateSanFrancisco() {
+        // given
         OfficeDto requestDto = TestDataProvider.getSanFranciscoOfficeDto();
         Office sanFrancisco = TestDataProvider.getSanFranciscoOfficeModel();
         when(officeMapper.officeDto2Office(requestDto)).thenReturn(sanFrancisco);
         when(officeManager.modify(sanFrancisco)).thenReturn(sanFrancisco);
         when(officeMapper.office2officeDto(sanFrancisco)).thenReturn(requestDto);
-
+        // when
         OfficeDto expected = TestDataProvider.getSanFranciscoOfficeDto();
 
         OfficeDto actual = controller.update(requestDto);
-
+        // then
         assertThat(actual).usingRecursiveComparison()
                 .isEqualTo(expected);
     }
 
     @Test
     void deleteFromQueryParamSanFrancisco() throws OfficeNotFoundException {
+        // given
         Office sanFrancisco = TestDataProvider.getSanFranciscoOfficeModel();
         when(officeManager.readByOfficeCode(sanFrancisco.getOfficeCode())).thenReturn(sanFrancisco);
         doNothing().when(officeManager).delete(sanFrancisco);
-
+        // when then
         controller.delete(TestDataProvider.OFFICE_CODE_SAN_FRANCISCO);
     }
 
     @Test
     void deleteFromQueryParamWhenOfficeNotFound() throws OfficeNotFoundException {
+        // given
         final String notFoundOfficeCode = TestDataProvider.OFFICE_CODE_SAN_FRANCISCO;
         doThrow(new OfficeNotFoundException()).when(officeManager).readByOfficeCode(notFoundOfficeCode);
-
+        // when then
         assertThatThrownBy(() -> controller.delete(notFoundOfficeCode))
                 .isInstanceOf(ResponseStatusException.class);
     }
