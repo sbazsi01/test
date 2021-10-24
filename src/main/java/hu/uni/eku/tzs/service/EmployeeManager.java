0983cb67e3a30ce5e3f7701c13b4c59729449cc1
sbazsi01 <sulykbazsi01@gmail.com>
2021-10-24 23:29:00 +0200

    fix(#13): fix checkstyle errors

--- src/main/java/hu/uni/eku/tzs/service/EmployeeManager.java
+++ src/main/java/hu/uni/eku/tzs/service/EmployeeManager.java
@@ -1,11 +1,8 @@
 package hu.uni.eku.tzs.service;
 
 import hu.uni.eku.tzs.model.Employee;
-import hu.uni.eku.tzs.model.Office;
 import hu.uni.eku.tzs.service.exceptions.EmployeeAlreadyExistsException;
 import hu.uni.eku.tzs.service.exceptions.EmployeeNotFoundException;
-import hu.uni.eku.tzs.service.exceptions.OfficeNotFoundException;
-
 import java.util.Collection;
 
 public interface EmployeeManager {
