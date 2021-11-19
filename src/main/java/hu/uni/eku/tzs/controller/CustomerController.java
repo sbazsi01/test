f57bad2ddf9ae579e4f75bcde3bfa3a3592c133d
sbazsi01 <sulykbazsi01@gmail.com>
2021-11-19 21:53:49 +0100

    fix(#23): fix checkstyle error

--- src/main/java/hu/uni/eku/tzs/controller/CustomerController.java
+++ src/main/java/hu/uni/eku/tzs/controller/CustomerController.java
@@ -9,7 +9,6 @@ import hu.uni.eku.tzs.service.exceptions.CustomerNotFoundException;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
