6bfe51df82a9600432a27f54b85dd0a4cc5ab213
kissolga0904 <qlcbh6@std.uni-eszterhazy.hu>
2021-11-22 10:53:28 +0100

    feat(#26): implemented dao

---
+++ src/main/java/hu/uni/eku/tzs/dao/entity/PaymentEntity.java
@@ -0,0 +1,35 @@
+package hu.uni.eku.tzs.dao.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Column;
+import java.sql.Date;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "payments")
+public class PaymentEntity {
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "customers", nullable = false)
+    private CustomerEntity customerNumber;
+
+    @Id
+    private String checkNumber;
+
+    @Column
+    Date paymentDate;
+
+    @Column
+    private double amount;
+}
