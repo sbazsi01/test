d137b56223911078c37ff54ea2bfdbe137655532
Patrik Csik <csik.patrik455@gmail.com>
2021-11-27 12:39:59 +0100

    Check style fix

--- src/main/java/hu/uni/eku/tzs/dao/OrderDetailRepository.java
+++ src/main/java/hu/uni/eku/tzs/dao/OrderDetailRepository.java
@@ -1,8 +1,6 @@
 package hu.uni.eku.tzs.dao;
 
 import hu.uni.eku.tzs.dao.entity.OrderDetailEntity;
-import hu.uni.eku.tzs.dao.entity.OrderDetailId;
-import hu.uni.eku.tzs.model.OrderDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrderDetailRepository extends JpaRepository<OrderDetailEntity, Integer> {
