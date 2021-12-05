18765ab5235d363f390d724d2dccd73099bff18a
Patrik Csik <csik.patrik.w@gmail.com>
2021-12-05 15:53:02 +0100

    feat(29): private - public

--- src/main/java/hu/uni/eku/tzs/service/OrderDetailManagerImpl.java
+++ src/main/java/hu/uni/eku/tzs/service/OrderDetailManagerImpl.java
@@ -21,7 +21,8 @@ public class OrderDetailManagerImpl implements OrderDetailManager {
 
     private static OrderDetail convertOrderDetailEntity2Model(OrderDetailEntity orderDetailEntity) {
         return new OrderDetail(
-                OrderManagerImpl.convertOrderEntity2Model(orderDetailEntity.getOrderNumber()),
+                OrderManagerImpl.co
+                //OrderManagerImpl.convertOrderEntity2Model(orderDetailEntity.getOrderNumber()),
                 ProductManagerImpl.convertProductEntity2Model(orderDetailEntity.getProductCode()),
                 orderDetailEntity.getQuantityOrdered(),
                 orderDetailEntity.getPriceEach(),
